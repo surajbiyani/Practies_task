87b6bc0ff0de881ccd8f392ab6ed517c61a0381d
surajbiyani <surajbiyani72@gmail.com>
2026-03-23 12:36:31 +0530

    Updated website files created navabar and hero section

---
+++ orangebits-clone/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./sections/Hero.jsx";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
